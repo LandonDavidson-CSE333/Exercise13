a697fce828c5d68abd426481f2910837bbd75482
Landon Davidson <accounts@landond.tech>
2025-02-17 14:50:48 -0800

    Add: rough draft Cartesian

--- Cartesian.cc
+++ Cartesian.cc
@@ -4,5 +4,25 @@
 
 #include "Cartesian.h"
 
+#include <valarray>
+
 namespace Cartesian {
+  double Cartesian::Distance(const Cartesian &cart) const {
+    // Find the distance between each component
+    double x_dist = cart.x_ - x_;
+    double y_dist = cart.y_ - y_;
+
+    // Square the components
+    x_dist *= x_dist;
+    y_dist *= y_dist;
+
+    // Return distance using pythagorean theorem
+    return std::sqrt(x_dist + y_dist);
+  }
+
+  std::string Cartesian::ToString() override {
+    return {};
+  }
+
+
 }  // Cartesian
--- Cartesian.h
+++ Cartesian.h
@@ -1,18 +1,30 @@
-//
-// Created by landond on 2/17/25.
-//
+// Copyright 2025 Landon Davidson
+// landond@uw.edu
 
-#ifndef CARTESIAN_H
-#define CARTESIAN_H
+#ifndef CARTESIAN_H_
+#define CARTESIAN_H_
 
 #include "Coordinate.h"
+#include <string>
 
 namespace Cartesian {
 
-class Cartesian {
+class Cartesian : Coordinate::Coordinate {
+ public:
+  // Default and two arg constructors
+  Cartesian() : x_(0), y_(0) {}
+  Cartesian(double x, double y) : x_(x), y_(y) {}
 
-};
+  // Return string representation of cartesian coord
+  std::string ToString() override;
+  // Return the distance between this and another cartesian
+  double Distance(const Cartesian &cart) const;
 
-} // Cartesian
+ private:
+  double x_;
+  double y_;
+};  // class Cartesian
 
-#endif //CARTESIAN_H
+}  // namespace Cartesian
+
+#endif  // CARTESIAN_H_
