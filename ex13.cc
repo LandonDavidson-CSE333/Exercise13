79cee97d53766039a7882f76e7d0021f5f7882da
Landon Davidson <accounts@landond.tech>
2025-02-17 15:39:30 -0800

    Refactor: print outs

--- ex13.cc
+++ ex13.cc
@@ -13,12 +13,12 @@ int main(int argc, char** argv) {
   const Cartesian::Cartesian cart1(10, 6);
   const Cartesian::Cartesian cart2(6, 3);
   std::cout << "Distance between (10, 6) and (6, 3) = " << cart1.Distance(cart2) << "\n";
-  std::cout << "(10,6): " << cart1.ToString() << "\n";
+  std::cout << "cart1: " << cart1.ToString() << "\n";
 
   std::cout << "\n--Polar--\n";
   // Create two polar points then test Distance and ToString
   const Polar::Polar pol1(10, 0);
   const Polar::Polar pol2(10, M_PI);
   std::cout << "Distance between (10, 0) and (10, pi) = " << pol1.Distance(pol2) << "\n";
-  std::cout << "(10, pi): " << pol2.ToString() << "\n";
+  std::cout << "pol2: " << pol2.ToString() << "\n";
 }
