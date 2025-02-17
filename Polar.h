2cf52678f5a7a75692fc34d25b2f2c8642f7c768
Landon Davidson <accounts@landond.tech>
2025-02-17 15:07:56 -0800

    Add: rough draft Polar.h

--- Polar.h
+++ Polar.h
@@ -5,12 +5,29 @@
 #ifndef POLAR_H
 #define POLAR_H
 
+#include <string>
+
 #include "Coordinate.h"
 
 namespace Polar {
 
-class Polar {
+class Polar : Coordinate::Coordinate {
+ public:
+  // Default and two arg constructor
+  Polar() : radius_(0), angle_(0) {}
+  Polar(double radius, double angle) : radius_(radius), angle_(angle) {}
 
+  // Represent Polar as a string
+  std::string ToString() override {
+    return "(" + std::to_string(radius_) + "," + std::to_string(angle_) + ")";
+  }
+
+  // Return the distance between this and another polar object
+  double Distance(const Polar& polar) const;
+
+ private:
+  double radius_;
+  double angle_;
 };
 
 } // Polar
